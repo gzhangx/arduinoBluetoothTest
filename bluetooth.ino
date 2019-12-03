afcd48aed016bcc498dd816e8ad35ea5121e97bb
Gang Zhang <gzhangx@hotmail.com>
2019-12-02 22:29:14 -0500

    adding servo code

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,10 +1,17 @@
 
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(11, 10); // RX | TX
+SoftwareSerial BTSerial(2, 3); // RX | TX (11 direct 10 R)
  
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+
+
 // Change DEBUG to true to output debug information to the serial monitor
 boolean DEBUG = true;
- 
+
+int myservoPos = 90;
+int wantedPos = 90; 
+
 void setup()  
 {
     if (DEBUG)
@@ -20,7 +27,9 @@ void setup()
     //  open software serial connection to the Bluetooth module.
     BTSerial.begin(9600); //9600
     if (DEBUG)  {   Serial.println("BTserial started at 38400");     }
- 
+
+  myservo.attach(5);
+  myservo.write(myservoPos);  
 } // void setup()
  
 
@@ -28,11 +37,20 @@ char sendstr[128];
 int sendstrpos = 0;
  char dbgstr[64];
  char dbgstr128[128];
+ int step= 10;
  unsigned long lastAvailableTime = millis();
 void loop()  
 {
      if (BTSerial.available()){
-        Serial.write(BTSerial.read());    
+        int c = BTSerial.read();
+        Serial.write(c);    
+        if (c == 'A') {
+          wantedPos-=step;
+          if (wantedPos < 0) wantedPos = 0;
+        }else if (c == 'D') {
+          wantedPos+=step;
+          if (wantedPos > 180) wantedPos = 180;
+        }
      }
   // Keep reading from Arduino Serial Monitor and send to HC-05
   if (Serial.available()){
@@ -40,6 +58,7 @@ void loop()
     sendstr[sendstrpos++] =c;
     sendstr[sendstrpos] =0;    
     lastAvailableTime = millis();
+    
   }else {
     if (sendstrpos && (millis()-lastAvailableTime)>500  ) {
       for (int i = 0; i < sendstrpos; i++)
@@ -50,5 +69,18 @@ void loop()
       sendstrpos = 0;
     }
     
+  }
+
+
+  servoControl();
+}
+
+
+void servoControl() {
+  if (wantedPos != myservoPos) {
+    myservoPos = wantedPos;
+    BTSerial.write(myservoPos);
+    BTSerial.write('\n');
+    myservo.write(myservoPos); 
   }
 }
