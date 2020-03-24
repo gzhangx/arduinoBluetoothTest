6d03ff6d5f418ac5529a4fc24f431734d7559df3
Gang Zhang <gzhangx@hotmail.com>
2020-03-24 19:30:40 -0400

    'test'

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,11 +1,11 @@
 
 #include <SoftwareSerial.h>
+#include "GArduinoBufUtil.h"
 SoftwareSerial BTSerial(2, 10); // RX | TX (11 direct 10 R)
 
 long BTBAUD = 57600 ; //4:9600 5:19200, 6:38400, 7:57600, 8: 115200 (not stable)
-char readBuffer[128];
-int readPos = 0;
-byte termFound = 0;
+
+RecBuf serBuf, blueBuf;
 void setup()  
 {
     Serial.begin(115200);
@@ -21,25 +21,18 @@ void loop()
 {
   if (BTSerial.available()){
      int c = BTSerial.read();
-     Serial.write(c);
+     if (blueBuf.onRecv(c)) {
+      Serial.println(blueBuf.origVal);
+     }
   }
   // Keep reading from Arduino Serial Monitor and send to HC-05
 
   
   if (Serial.available()){
     int c = Serial.read();
-    readBuffer[readPos++] = (char)c;
-    if (c == '\n' || readPos > 100) {
-      termFound = 1;
+    if (serBuf.onRecv(c)) {      
+      BTSerial.write(serBuf.val.c_str());
+      BTSerial.write("\r\n");
     }
-  }
-  if (termFound) {
-    termFound = 0;
-    for(int i = 0; i < readPos;i++) {
-      int c = readBuffer[i];
-      Serial.write(c);
-      BTSerial.write(c);
-    }
-    readPos = 0;
-  }
+  }  
 }
